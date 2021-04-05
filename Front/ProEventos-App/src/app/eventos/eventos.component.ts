ddfb74b669c699019244be3f066740db662fad1d
tiautocom <tiautocomp@gmail.com>
2021-04-05 16:33:46 -0300

    seção5: Angular, Interpolação, diretivas e Binding

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,8 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss']
 })
 export class EventosComponent implements OnInit {
-public eventos: any;
+public eventos: any = [];
+public eventosFiltrados: any = [];
+marginImg = 2;
+widthImg = 150;
+mostrarImagem = true;
+// tslint:disable-next-line:variable-name
+private _filtroLista = '';
 
+public get filtroLista(): string {
+  return this._filtroLista;
+}
+
+public set filtroLista(value: string) {
+  this._filtroLista = value;
+  this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+}
+
+filtrarEventos(filtrarPor: string): any{
+  filtrarPor = filtrarPor.toLocaleLowerCase();
+  return this.eventos.filter(
+    evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+    evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+  );
+}
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
@@ -18,10 +40,19 @@ public eventos: any;
 
   public getEventos(): void{
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
       // tslint:disable-next-line:no-shadowed-variable
       error => console.log(error)
     );
+  }
+
+  // tslint:disable-next-line:typedef
+  public alterarImagem(){
+    // tslint:disable-next-line:no-unused-expression
+    this.mostrarImagem = !this.mostrarImagem;
   }
 
 }
